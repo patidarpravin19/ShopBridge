a3b0c3811d7624b7bf4a9d83a631ff011308366c
Vivek_Ionetic <nandan@test.com>
2021-07-24 18:10:39 +0530

    Committing complete project

---
+++ ShopBridge.Entities/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopBridge.Entities
+{
+    public class Product : BaseEntity
+    {
+        public int Name { get; }
+        public int Description { get; }
+    }
+}
---
+++ ShopBridge/App_Start/WebApiConfig.cs
@@ -0,0 +1,30 @@
+﻿using FluentValidation.WebApi;
+using ShopBridge.App_Start;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace ShopBridge
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            // Add Custom validation filters  
+            config.Filters.Add(new ValidateModelStateFilter());
+            FluentValidationModelValidatorProvider.Configure(config);
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{action}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
